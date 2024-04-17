1068fdcf3e92628081274a81a280253483dc2b82
Breymar34 <79845808+Breymar34@users.noreply.github.com>
2024-04-17 10:40:58 +0800

    *

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'davmixcool/php-sentiment-analyzer' => array(
+            'pretty_version' => '1.2.2',
+            'version' => '1.2.2.0',
+            'reference' => 'd7503b68313c62eee3ed0ac9774c0a84d818a405',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../davmixcool/php-sentiment-analyzer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nlp-tools/nlp-tools' => array(
+            'pretty_version' => 'v0.1.3',
+            'version' => '0.1.3.0',
+            'reference' => 'bbccab0f3ca7277bdf7b292ba67ca644efd0cefe',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nlp-tools/nlp-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
